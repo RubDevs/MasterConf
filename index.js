fc8d8aa8ee8c7ad793c5c82dbc830e85b9bd6137
Rubén Hernández <69823633+RubDevs@users.noreply.github.com>
2021-04-16 15:02:53 -0500

    API docs using swagger (#11)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
 
 const config = require("./config");
 const conference = require("./components/conference/network");
@@ -7,6 +8,7 @@ const hotel = require("./components/hotel/network");
 const food = require("./components/food/network");
 const location = require("./components/location/netwotk");
 const errors = require("./network/errors");
+const swaggerDoc = require("./swagger.json");
 
 const app = express();
 
@@ -18,6 +20,7 @@ app.use("/", conference);
 app.use("/hotel", hotel);
 app.use("/food", food);
 app.use("/location", location);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(errors);
 
 app.listen(process.env.PORT || config.api.port, () => {
